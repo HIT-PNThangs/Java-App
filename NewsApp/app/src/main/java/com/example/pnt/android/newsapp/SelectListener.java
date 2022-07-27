8f1246e3de7ac449b4638e931a6b41cc583274e8
HIT-PNThangs <p.n.thang0101@gmail.com>
2022-07-28 02:36:22 +0700

    28/07/2022

--- NewsApp/app/src/main/java/com/example/pnt/android/newsapp/SelectListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.pnt.android.newsapp;
-
-import com.example.pnt.android.newsapp.Models.NewsHeadlines;
-
-public interface SelectListener {
-    void OnNewsClicked(NewsHeadlines headlines);
-}
